3fd2bd506c3c065b2add3294d14f648b8615ec4b
Gaurav <19ucs246@lnmiit.ac.in>
2022-04-13 23:42:44 +0530

    First Commit

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react"; 
+
+export default function Header() {
+    return (
+        <header className="header">
+            <img src="https://upload.wikimedia.org/wikipedia/en/9/9a/Trollface_non-free.png" alt="Troll Face" className="header--image"></img>
+            <h2 className="header--title">Meme Generator</h2>
+            <h2 className="header--project">Hello Memers!!</h2>
+        </header> 
+    )
+}
